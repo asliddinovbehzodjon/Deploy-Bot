3813b0ab8f4cd50ad5a5d359a119b3d6ed4a7552
Behzod <behzodtuit@gmail.com>
2022-11-28 20:00:11 +0500

    deploy-bot

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -4,6 +4,9 @@ from loader import dp
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Assalomu alaykum, {message.from_user.full_name}!")
+@dp.message_handler(commands='behzod')
+async def test(message:types.Message):
+    await message.answer("Behzod buyrugini berdiz!")
 @dp.message_handler(content_types='text')
 async def test(message:types.Message):
     key = message.text
@@ -17,6 +20,3 @@ async def test(message:types.Message):
     except wikipedia.exceptions.PageError:
         result ="Malumot topilmadi!"
     await message.answer(result)
-@dp.message_handler(commands='behzod')
-async def test(message:types.Message):
-    await message.answer("Behzod buyrugini berdiz!")
