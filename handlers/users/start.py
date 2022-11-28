e253c2c03e522acbecf08e3d5ea202d70a0abb49
Behzod <behzodtuit@gmail.com>
2022-11-28 19:36:53 +0500

    deploy-bot

---
+++ handlers/users/start.py
@@ -0,0 +1,19 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandStart
+from loader import dp
+@dp.message_handler(CommandStart())
+async def bot_start(message: types.Message):
+    await message.answer(f"Assalomu alaykum, {message.from_user.full_name}!")
+@dp.message_handler(content_types='text')
+async def test(message:types.Message):
+    key = message.text
+    import wikipedia
+    wikipedia.set_lang('uz')
+    result = ''
+    try:
+        result = wikipedia.summary(key)
+    except wikipedia.exceptions.DisambiguationError:
+        result = 'Bu kalit boyicha malumot kop.Iltimos batafsil kiriting'
+    except wikipedia.exceptions.PageError:
+        result ="Malumot topilmadi!"
+    await message.answer(result)
